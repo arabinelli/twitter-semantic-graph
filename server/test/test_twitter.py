75e8ef8780c5d6127e33d01ee6f66104574ea686
arabinelli <andrea.rabinelli@gmail.com>
2020-08-10 19:32:51 +0200

    test(twitter) - refactoring on tests

--- server/test/test_twitter.py
+++ server/test/test_twitter.py
@@ -5,15 +5,6 @@ from twitter.client import TwitterClient
 twitter_client = TwitterClient()
 
 
-def test_twitter_client_can_retreive_env_variables():
-    twitter_client._read_keys_from_env_variables()
-
-
-def test_twitter_client_can_successfully_authenticate():
-    twitter_client._authenticate()
-    twitter_client.api.verify_credentials()
-
-
 def test_twitter_client_cannot_create_query_without_hashtags():
     with pytest.raises(ValueError):
         hashtags = []
@@ -50,3 +41,12 @@ def test_twitter_client_can_create_query_filtering_retweets():
     hashtags = ["#onehashtag", "#otherhashtag"]
     query = twitter_client._build_query(hashtags, filter_retweets=True)
     assert query == "#onehashtag OR #otherhashtag -filter:retweets"
+
+
+def test_twitter_client_can_retreive_env_variables():
+    twitter_client._read_keys_from_env_variables()
+
+
+def test_twitter_client_can_successfully_authenticate():
+    twitter_client._authenticate()
+    twitter_client.api.verify_credentials()

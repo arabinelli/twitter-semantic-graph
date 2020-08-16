112ed1c858a9592cd5a8352095c8eab826e74ef9
arabinelli <andrea.rabinelli@gmail.com>
2020-08-16 12:37:34 +0200

    feat(server/api) - add CORS and updated request body parameters

--- server/src/main.py
+++ server/src/main.py
@@ -4,15 +4,27 @@ from pydantic import BaseModel
 
 from twitter.client import TwitterClient
 from network.builder import NetworkBuilder
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["http://localhost", "http://localhost:3000", "http://localhost:3001"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class GraphRequest(BaseModel):
     hashtags: List[str]
     languages: Optional[List[str]] = None
     filter_retweets: Optional[bool] = True
-    filter_frequency: Optional[int] = 0
+    filter_node_frequency: Optional[int] = 0
+    filter_link_frequency: Optional[int] = 0
 
 
 def make_graph(request: GraphRequest):
@@ -27,10 +39,17 @@ def make_graph(request: GraphRequest):
     print("Building the graph...")
     network_builder = NetworkBuilder()
     network_builder.load_clean_corpus(corpus)
-    graph = network_builder.build_graph(filter_term_frequency=request.filter_frequency)
+    keywords_to_remove = request.hashtags if len(request.hashtags) == 1 else []
+    graph = network_builder.build_graph(
+        filter_node_frequency=request.filter_node_frequency,
+        filter_link_frequency=request.filter_link_frequency,
+        keywords_to_remove=keywords_to_remove,
+    )
     return graph
 
 
 @app.post("/get-graph")
 async def root(request: GraphRequest):
+    print("received")
+    print(request)
     return make_graph(request)

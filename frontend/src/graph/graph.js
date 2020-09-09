9892dedde86673f18fafbefcb45b3972daf6d9ee
arabinelli <andrea.rabinelli@gmail.com>
2020-09-09 12:55:53 +0200

    feat: viz - change to react-force-graph-2d library

--- frontend/src/graph/graph.js
+++ frontend/src/graph/graph.js
@@ -1,21 +1,20 @@
-import React, { useState, useEffect } from "react";
-import { Graph } from "react-d3-graph";
-// import data from "../../sample_data/graph_data_filtered.json";
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import { log2, sqrt, max } from "mathjs";
+import ForceGraph2D from "react-force-graph-2d";
+import LoadScreen from "../components/loading/loading";
+import useWindowDimensions from "../utils/windowSize";
+import "./graph.css";
 
-const NetworkViz = () => {
-  // graph payload (with minimalist structure)
-  //   data = {
-  //     nodes: [{ id: "Harry" }, { id: "Sally" }, { id: "Alice" }],
-  //     links: [
-  //       { source: "Harry", target: "Sally" },
-  //       { source: "Harry", target: "Alice" },
-  //     ],
-  //   };
-
-  const [status, setStatus] = useState("idle");
+const NetworkViz = (props) => {
   const [hasError, setErrors] = useState(false);
   const [dataHasLoaded, setDataLoaded] = useState(false);
   const [data, setData] = useState({ nodes: [], links: [] });
+  const [highlightNodes, setHighlightNodes] = useState(new Set());
+  const [highlightLinks, setHighlightLinks] = useState(new Set());
+  const [hoverNode, setHoverNode] = useState(null);
+
+  const forceRef = useRef(null);
+  const { height, width } = useWindowDimensions();
 
   useEffect(() => {
     const abortcontroller = new AbortController();
@@ -25,16 +24,29 @@ const NetworkViz = () => {
       await fetch("http://localhost/get-graph", {
         method: "POST",
         body: JSON.stringify({
-          hashtags: ["#digitalhealth"],
-          languages: ["en"],
-          filter_node_frequency: 0,
-          filter_link_frequency: 0,
+          hashtags: [props.hashtags],
+          languages: [props.language],
+          filter_node_frequency: 1,
+          filter_link_frequency: 1,
         }),
         signal: signal,
       })
         .then((res) => res.json())
         .then((data) => {
           console.log("This is your data", data);
+          data.links.forEach((link) => {
+            const a = data.nodes[link.source];
+            const b = data.nodes[link.target];
+            !a.neighbors && (a.neighbors = []);
+            !b.neighbors && (b.neighbors = []);
+            a.neighbors.push(b);
+            b.neighbors.push(a);
+
+            !a.links && (a.links = []);
+            !b.links && (b.links = []);
+            a.links.push(link);
+            b.links.push(link);
+          });
           setData(data);
           setDataLoaded(true);
         })
@@ -43,100 +55,116 @@ const NetworkViz = () => {
           setErrors(true);
         });
 
-      // try {
-      //   const data = await res.json();
-      //   setData(data);
-      // } catch (error) {
-      //   console.log(error);
-      // }
-
       return function cleanup() {
         abortcontroller.abort();
       };
     }
     console.log("Fetching!");
-    setStatus("fetching");
     fetchGraphData();
   }, []);
 
-  // the graph configuration, you only need to pass down properties
-  // that you want to override, otherwise default ones will be used
-  const myConfig = {
-    nodeHighlightBehavior: true,
-    height: window.innerHeight,
-    width: window.innerWidth,
-    d3: { gravity: -150, linkStrength: 0.3 },
-    node: {
-      color: "#a8dadc",
-      size: 120,
-      highlightStrokeColor: "blue",
-      labelProperty: "name",
-      fontColor: "white",
+  useEffect(() => {
+    if (forceRef.current != null) {
+      forceRef.current.d3Force("charge").strength(-300);
+      forceRef.current.d3Force("link").distance(250);
+    }
+  });
+
+  const nodeObject = useCallback(
+    (node, ctx, globalScale) => {
+      const label = node.name;
+      const fontSize = max(10, 3 * sqrt(node.size)) / globalScale;
+      ctx.font = highlightNodes.has(node)
+        ? `${fontSize}px Montserrat`
+        : `${fontSize}px Montserrat`;
+      const textWidth = ctx.measureText(label).width;
+      const bckgDimensions = [textWidth, fontSize].map(
+        (n) => n + fontSize * 0.2
+      ); // some padding
+      if (highlightNodes.size === 0) {
+        ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
+      } else {
+        ctx.fillStyle = highlightNodes.has(node)
+          ? "rgba(0, 0, 0, 0.3)"
+          : "rgba(0, 0, 0, 0)";
+      }
+      ctx.fillRect(
+        node.x - bckgDimensions[0] / 2,
+        node.y - bckgDimensions[1] / 2,
+        ...bckgDimensions
+      );
+
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      if (highlightNodes.size === 0) {
+        ctx.fillStyle = "#a8dadc";
+      } else {
+        ctx.fillStyle = highlightNodes.has(node) ? "#a8dadc" : "#303030";
+      }
+
+      ctx.fillText(label, node.x, node.y);
     },
-    link: {
-      highlightColor: "lightblue",
-      color: "#AAAAAA",
-      opacity: 0.5,
-    },
+    [highlightNodes]
+  );
+
+  const updateHighlight = () => {
+    setHighlightNodes(highlightNodes);
+    setHighlightLinks(highlightLinks);
   };
 
-  // // graph event callbacks
-  // const onClickGraph = function () {
-  //   window.alert(`Clicked the graph background`);
-  // };
+  const handleNodeHover = (node) => {
+    highlightNodes.clear();
+    highlightLinks.clear();
+    if (node) {
+      highlightNodes.add(node);
+      node.neighbors.forEach((neighbor) => highlightNodes.add(neighbor));
+      node.links.forEach((link) => highlightLinks.add(link));
+    }
 
-  // const onClickNode = function (nodeId) {
-  //   window.alert(`Clicked node ${nodeId}`);
-  // };
+    setHoverNode(node || null);
+    updateHighlight();
+  };
 
-  // const onDoubleClickNode = function (nodeId) {
-  //   window.alert(`Double clicked node ${nodeId}`);
-  // };
+  const handleLinkHover = (link) => {
+    highlightNodes.clear();
+    highlightLinks.clear();
 
-  // const onRightClickNode = function (event, nodeId) {
-  //   window.alert(`Right clicked node ${nodeId}`);
-  // };
+    if (link) {
+      highlightLinks.add(link);
+      highlightNodes.add(link.source);
+      highlightNodes.add(link.target);
+    }
 
-  // const onMouseOverNode = function (nodeId) {
-  //   window.alert(`Mouse over node ${nodeId}`);
-  // };
-
-  // const onMouseOutNode = function (nodeId) {
-  //   window.alert(`Mouse out node ${nodeId}`);
-  // };
-
-  // const onClickLink = function (source, target) {
-  //   window.alert(`Clicked link between ${source} and ${target}`);
-  // };
-
-  // const onRightClickLink = function (event, source, target) {
-  //   window.alert(`Right clicked link between ${source} and ${target}`);
-  // };
-
-  // const onMouseOverLink = function (source, target) {
-  //   window.alert(`Mouse over in link between ${source} and ${target}`);
-  // };
-
-  // const onMouseOutLink = function (source, target) {
-  //   window.alert(`Mouse out link between ${source} and ${target}`);
-  // };
-
-  // const onNodePositionChange = function (nodeId, x, y) {
-  //   window.alert(
-  //     `Node ${nodeId} is moved to new position. New position is x= ${x} y= ${y}`
-  //   );
-  // };
+    updateHighlight();
+  };
 
   return (
     <>
       {dataHasLoaded === true ? (
-        <Graph
-          id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
-          data={data}
-          config={myConfig}
-        />
+        <div className="network">
+          <ForceGraph2D
+            height={height * 0.9}
+            width={width}
+            graphData={data}
+            nodeLabel="name"
+            nodeRelSize={6}
+            linkColor={(link) => {
+              if (highlightLinks.size === 0) {
+                return "#AAAAAA40";
+              } else {
+                return highlightLinks.has(link) ? "#AAAAAA80" : "#AAAAAA20";
+              }
+            }}
+            linkWidth={(link) => (highlightLinks.has(link) ? 2 : 1)}
+            linkOpacity={0.3}
+            nodeCanvasObject={nodeObject}
+            d3AlphaMin={0.03}
+            ref={forceRef}
+            onNodeHover={handleNodeHover}
+          />
+        </div>
       ) : (
-        <p>Data is loading. Please wait...</p>
+        <LoadScreen />
       )}
     </>
   );

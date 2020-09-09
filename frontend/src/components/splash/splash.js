ade56589f2ee198de1e195cc93af2f29297894cf
arabinelli <andrea.rabinelli@gmail.com>
2020-09-09 12:54:30 +0200

    feat: splash page component
    this is a WIP

---
+++ frontend/src/components/splash/splash.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./splash.css";
+
+const SplashScreen = () => {
+  return (
+    <div className="welcome">
+      <p>What are you gonna search today?</p>
+      <p>Open the side menu to start...</p>
+    </div>
+  );
+};
+
+export default SplashScreen;

896a39da363def3f20e826d864555a4fe09a1dba
arabinelli <andrea.rabinelli@gmail.com>
2020-09-09 19:10:12 +0200

    refactor: webapp - move drawer and header in components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,111 +2,23 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import NetworkViz from "./graph/graph";
 import SplashScreen from "./components/splash/splash";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-import IconButton from "@material-ui/core/IconButton";
-import MenuOpenIcon from "@material-ui/icons/MenuOpen";
-import MenuIcon from "@material-ui/icons/Menu";
-import {
-  Box,
-  Container,
-  Typography,
-  AppBar,
-  Toolbar,
-  Button,
-  Menu,
-  Drawer,
-  TextField,
-  MenuItem,
-} from "@material-ui/core";
+import AppHeader from "./components/appHeader/appHeader";
+import AppDrawer from "./components/drawer/drawer";
 
-const drawerWidth = "20vw";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-    height: "100vh",
-  },
-  drawer: {
-    padding: theme.spacing(1),
-    width: drawerWidth,
-    flexShrink: 0,
-    // height: "90vh",
-    // alignContent: "bottom",
-    "& .MuiTextField-root": {
-      width: "18vw",
-    },
-    "& .MuiButton-root": {
-      backgroundColor: "#d10019",
-    },
-    "& .MuiButton-label": {
-      color: "#f1faee",
-    },
-  },
-  drawerPaper: {
-    width: drawerWidth,
-    backgroundColor: "#f1faee",
-  },
-
-  inputFields: {
-    margin: theme.spacing(2),
-    marginTop: theme.spacing(3),
-  },
-  drawerTitle: {
-    margin: theme.spacing(2),
-  },
-  appBar: {
-    backgroundColor: "#457b9d",
-    // zIndex: theme.zIndex.drawer + 100,
-    zIndex: 1400,
-  },
-  toolbar: theme.mixins.toolbar,
 }));
 
-const languages = [
-  {
-    value: "",
-    label: "Any",
-  },
-  {
-    value: "en",
-    label: "English",
-  },
-  {
-    value: "it",
-    label: "Italian",
-  },
-  {
-    value: "de",
-    label: "German",
-  },
-  {
-    value: "fr",
-    label: "French",
-  },
-  {
-    value: "es",
-    label: "Spanish",
-  },
-];
-
 function App() {
-  const [formIsSubmitted, setFormIsSubmitted] = useState(false);
+  const [formIsSubmitted, setFormIsSubmitted] = useState(0);
   const [typedHashtag, setTypedHashtag] = useState("");
   const [isMenuOpen, setMenuOpen] = useState(false);
   const [language, setLanguage] = useState("");
+  const [searchResult, setSearchResult] = useState("");
 
   const classes = useStyles();
 
@@ -118,91 +30,41 @@ function App() {
     setLanguage(event.target.value);
   };
 
+  const handleHashtagChange = (event) => {
+    setTypedHashtag(event.target.value);
+  };
+
   const handleFormSubmit = (event) => {
-    setFormIsSubmitted(false);
     event.preventDefault();
-    console.log(typedHashtag);
-    console.log(language);
-    setFormIsSubmitted(true);
+    console.log("In handleFormSubmit:", typedHashtag);
+    console.log("In handleFormSubmit:", language);
+    setFormIsSubmitted(formIsSubmitted + 1);
     toggleDrawer();
+    // API request here
+    // set search results
   };
 
   return (
     <div className={classes.root}>
-      <AppBar position="relative" className={classes.appBar}>
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-            onClick={toggleDrawer}
-          >
-            {isMenuOpen ? <MenuOpenIcon /> : <MenuIcon />}
-          </IconButton>
-          <Typography variant="h5" className={classes.title}>
-            Twitter Hashtag Mapper
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        anchor="left"
-        open={isMenuOpen}
-        onClose={toggleDrawer}
-        className={classes.drawer}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.toolbar} />
-        <Typography variant="h6" className={classes.drawerTitle}>
-          What are you looking for?
-        </Typography>
-        <form
-          className={classes.textFields}
-          noValidate
-          autoComplete="off"
-          onSubmit={handleFormSubmit}
-        >
-          <div className={classes.inputFields}>
-            <TextField
-              id="standard-basic"
-              label="Hashtags"
-              multiline
-              value={typedHashtag}
-              onChange={(e) => {
-                setTypedHashtag(e.target.value);
-              }}
-            />
-          </div>
-          <div className={classes.inputFields}>
-            <TextField
-              id="select-language"
-              select
-              label="Language"
-              value={language}
-              onChange={handleLanguageChoice}
-              helperText="Select the language you're interested in"
-            >
-              {languages.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </TextField>
-          </div>
-          <div className={classes.inputFields}>
-            <Button variant="contained" type="submit">
-              Get Graph!
-            </Button>
-          </div>
-        </form>
-      </Drawer>
+      <AppHeader isMenuOpen={isMenuOpen} toggleDrawerFunction={toggleDrawer} />
+      <AppDrawer
+        isMenuOpen={isMenuOpen}
+        toggleDrawerFunction={toggleDrawer}
+        handleFormSubmit={handleFormSubmit}
+        typedHashtag={typedHashtag}
+        handleHashtagChange={handleHashtagChange}
+        language={language}
+        handleLanguageChoice={handleLanguageChoice}
+      />
       <>
-        {formIsSubmitted === true ? (
-          <NetworkViz hashtags={typedHashtag} language={language} />
+        {formIsSubmitted ? (
+          <NetworkViz
+            hashtags={typedHashtag}
+            language={language}
+            formIsSubmitted={formIsSubmitted}
+          />
         ) : (
+          // change props to searchResults
           <SplashScreen />
         )}
       </>
---
+++ frontend/src/components/drawer/drawer.js
@@ -0,0 +1,137 @@
+import React from "react";
+import {
+  Typography,
+  Button,
+  Drawer,
+  TextField,
+  MenuItem,
+} from "@material-ui/core";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const AppDrawer = (props) => {
+  const drawerWidth = "20vw";
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(1),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+      height: "100vh",
+    },
+    drawer: {
+      padding: theme.spacing(1),
+      width: drawerWidth,
+      flexShrink: 0,
+      "& .MuiTextField-root": {
+        width: "18vw",
+      },
+      "& .MuiButton-root": {
+        backgroundColor: "#d10019",
+      },
+      "& .MuiButton-label": {
+        color: "#f1faee",
+      },
+    },
+    drawerPaper: {
+      width: drawerWidth,
+      backgroundColor: "#f1faee",
+    },
+    inputFields: {
+      margin: theme.spacing(2),
+      marginTop: theme.spacing(3),
+    },
+    drawerTitle: {
+      margin: theme.spacing(2),
+    },
+    toolbar: theme.mixins.toolbar,
+  }));
+
+  const classes = useStyles();
+
+  const languages = [
+    {
+      value: "",
+      label: "Any",
+    },
+    {
+      value: "en",
+      label: "English",
+    },
+    {
+      value: "it",
+      label: "Italian",
+    },
+    {
+      value: "de",
+      label: "German",
+    },
+    {
+      value: "fr",
+      label: "French",
+    },
+    {
+      value: "es",
+      label: "Spanish",
+    },
+  ];
+
+  return (
+    <Drawer
+      anchor="left"
+      open={props.isMenuOpen}
+      onClose={props.toggleDrawerFunction}
+      className={classes.drawer}
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+    >
+      <div className={classes.toolbar} />
+      <Typography variant="h6" className={classes.drawerTitle}>
+        What are you looking for?
+      </Typography>
+      <form
+        className={classes.textFields}
+        noValidate
+        autoComplete="off"
+        onSubmit={props.handleFormSubmit}
+      >
+        <div className={classes.inputFields}>
+          <TextField
+            id="standard-basic"
+            label="Hashtags"
+            multiline
+            value={props.typedHashtag}
+            onChange={props.handleHashtagChange}
+          />
+        </div>
+        <div className={classes.inputFields}>
+          <TextField
+            id="select-language"
+            select
+            label="Language"
+            value={props.language}
+            onChange={props.handleLanguageChoice}
+            helperText="Select the language you're interested in"
+          >
+            {languages.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        </div>
+        <div className={classes.inputFields}>
+          <Button variant="contained" type="submit">
+            Get Graph!
+          </Button>
+        </div>
+      </form>
+    </Drawer>
+  );
+};
+
+export default AppDrawer;

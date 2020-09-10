49b850000135914fd702973a4462220e9fd9265b
arabinelli <andrea.rabinelli@gmail.com>
2020-09-10 10:22:09 +0200

    feat: add p5js animated background

---
+++ frontend/src/components/animatedBackground/animatedBackground.js
@@ -0,0 +1,116 @@
+import React, { useRef, useEffect } from "react";
+import useWindowDimensions from "../../utils/windowSize";
+import p5 from "p5";
+
+const AnimatedBackground = (props) => {
+  var nParticles = 120;
+  var particles = [];
+  var particleSize = 70;
+  var maxCounter = 20;
+  var lines = [];
+  var speedFactor = 0.9;
+
+  const { height, width } = useWindowDimensions();
+
+  const processingRef = useRef();
+  const Sketch = (p) => {
+    p.checkCollision = () => {
+      lines = [];
+      for (var i = 0; i < nParticles; i++) {
+        for (var j = 0; j < nParticles; j++) {
+          if (i !== j) {
+            var distance = p5.Vector.dist(
+              particles[i].position,
+              particles[j].position
+            );
+            if (distance < particleSize) {
+              if (particles[i].counter === 0) {
+                particles[i].direction.rotate(Math.random());
+                particles[i].counter = maxCounter;
+              }
+              if (particles[j].counter === 0) {
+                particles[j].direction.rotate(Math.random());
+                particles[j].counter = maxCounter;
+              }
+              lines.push([
+                particles[i].position,
+                particles[j].position,
+                distance,
+              ]);
+            }
+          }
+        }
+      }
+    };
+
+    p.createParticle = () => {
+      var particle = {};
+
+      particle.position = p.createVector(
+        Math.random() * width,
+        Math.random() * height
+      );
+
+      particle.direction = p.createVector(
+        (Math.random() - 0.5) * 2 * speedFactor,
+        (Math.random() - 0.5) * 2 * speedFactor
+      );
+
+      particle.update = function () {
+        this.position.add(this.direction);
+        if (this.position.x > width || this.position.x < 0) {
+          //this.position.x = Math.random() * width;
+          this.direction.rotate(Math.random());
+        }
+        if (this.position.y > height || this.position.y < 0) {
+          //this.position.y = Math.random() * height;
+          this.direction.rotate(Math.random());
+        }
+        if (this.counter > 0) {
+          this.counter -= 1;
+        }
+      };
+
+      particle.counter = 0;
+
+      return particle;
+    };
+
+    p.setup = () => {
+      // use parent to render the canvas in this ref
+      // (without that p5 will render the canvas outside of your component)
+      p.createCanvas(width, height * 0.9);
+      p.stroke("#1d355780");
+      p.fill("#1d355790");
+
+      for (var i = 0; i < nParticles; i++) {
+        particles.push(p.createParticle());
+      }
+    };
+    console.log(particles);
+
+    p.draw = () => {
+      p.background("#0a131f");
+
+      p.checkCollision();
+      for (var i = 0; i < nParticles; i++) {
+        particles[i].update();
+        p.ellipse(particles[i].position.x, particles[i].position.y, 5);
+      }
+
+      for (var i = 0; i < lines.length; i++) {
+        var color = p.map(lines[i][2], 0, particleSize, 0, 255);
+        p.stroke("#1d355780");
+        p.line(lines[i][0].x, lines[i][0].y, lines[i][1].x, lines[i][1].y);
+      }
+    };
+  };
+
+  useEffect(() => {
+    const newp5 = new p5(Sketch, processingRef.current);
+  }, []);
+
+  return <div ref={processingRef} id="background-container" />;
+};
+
+export default AnimatedBackground;
--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./splash.css";
+import AnimatedBackground from "../animatedBackground/animatedBackground";
 
 const SplashScreen = () => {
   return (
-    <div className="welcome">
-      <p>What are you gonna search today?</p>
-      <p>Open the side menu to start...</p>
+    <div>
+      <AnimatedBackground />
+      <div className="welcome">
+        <p>What are you gonna search today?</p>
+        <p>Open the side menu to start...</p>
+      </div>
     </div>
   );
 };
